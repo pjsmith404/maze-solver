5489528f55b80790076ea1c1923a780d85629853
pjsmith404 <pjsmith404@gmail.com>
2025-03-30 20:33:22 +1100

    draw window with tkinter

--- main.py
+++ main.py
@@ -1,6 +1,30 @@
-def main():
-    print("Hello from maze-solver!")
+from tkinter import Tk, BOTH, Canvas
 
+class Window:
+    def __init__(self, width, height):
+        self.__root = Tk()
+        self.__root.title("Maze Solver")
+        self.canvas = Canvas(self.__root, width=width, height=height)
+        self.canvas.pack()
+        self.running = False
+        self.__root.protocol("WM_DELETE_WINDOW", self.close)
+
+    def redraw(self):
+        self.__root.update_idletasks()
+        self.__root.update()
+
+    def wait_for_close(self):
+        self.running = True
+        while self.running:
+            self.redraw()
+
+    def close(self):
+        self.running = False
+
+def main():
+    print("Running")
+    win = Window(800, 600)
+    win.wait_for_close()
 
 if __name__ == "__main__":
     main()
