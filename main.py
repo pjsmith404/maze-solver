2fca87f65c20869932ec2b34d35c19510529ca94
pjsmith404 <pjsmith404@gmail.com>
2025-03-30 20:59:15 +1100

    add line drawing functionality

--- main.py
+++ main.py
@@ -1,8 +1,13 @@
-from window import Window
+from random import randrange
+
+from window import Window, Line, Point
 
 def main():
     print("Running")
     win = Window(800, 600)
+    for i in range(10):
+        line = Line(Point(randrange(800),randrange(600)), Point(randrange(800),randrange(600)))
+        win.draw_line(line, "red")
     win.wait_for_close()
 
 if __name__ == "__main__":
